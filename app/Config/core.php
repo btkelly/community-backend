b0606dcaf5a213ead4896b123855b5b8896d4fe0
Bryan Kelly <bkelly@detroitlabs.com>
2014-09-30 22:32:32 -0400

    Updated default security salts

--- app/Config/core.php
+++ app/Config/core.php
@@ -222,12 +222,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', '23849unf23890fnu402938un4032fnu4f23890nfu8902');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '23749872394823794879823749823');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
