ea2af107ea99f1d648df82704567686d0a13fc07
Bryan Kelly <bkelly@detroitlabs.com>
2014-09-30 21:12:54 -0400

    Added database config

--- app/Config/database.php
+++ app/Config/database.php
@@ -62,27 +62,34 @@
  * flags =>
  * A key/value array of driver specific connection options.
  */
-class DATABASE_CONFIG {
-
-	public $default = array(
-		'datasource' => 'Database/Mysql',
-		'persistent' => false,
-		'host' => 'localhost',
-		'login' => 'user',
-		'password' => 'password',
-		'database' => 'database_name',
-		'prefix' => '',
-		//'encoding' => 'utf8',
-	);
-
-	public $test = array(
-		'datasource' => 'Database/Mysql',
-		'persistent' => false,
-		'host' => 'localhost',
-		'login' => 'user',
-		'password' => 'password',
-		'database' => 'test_database_name',
-		'prefix' => '',
-		//'encoding' => 'utf8',
-	);
-}
+    class DATABASE_CONFIG {
+        
+        function __construct() {
+            
+            $url = parse_url(getenv('CLEARDB_DATABASE_URL'));
+            
+            $this->default = array(
+                                   'datasource' => 'Database/Mysql',
+                                   'persistent' => false,
+                                   'host' => $url['host'],
+                                   'login' => $url['user'],
+                                   'password' => $url['pass'],
+                                   'database' => substr($url['path'],1),
+                                   'prefix' => '',
+                                   //'encoding' => 'utf8',
+                                   );
+            
+            $this->test = array(
+                                'datasource' => 'Database/Mysql',
+                                'persistent' => false,
+                                'host' => 'localhost',
+                                'login' => 'user',
+                                'password' => 'password',
+                                'database' => 'test_database_name',
+                                'prefix' => '',
+                                //'encoding' => 'utf8',
+                                );
+            
+        }
+        
+    }
